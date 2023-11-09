e0fbb497b4bd806899dfb32499d92db26eb49a72
Dominik Zagórnik <dominik.zagornik@protonmail.com>
2023-11-09 02:13:27 +0100

    Coin spawner fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
     [SerializeField] private GameObject coin;
+    [SerializeField] private GameObject terrain;
 
     [SerializeField] private float spawnOffset = 35;
     [SerializeField] private float enemySpawnRate = 5;
@@ -17,6 +18,8 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<Terrain>();
+
         for (int i = 0; i < 5; i++)
         {
             spawnEnemy();
@@ -59,6 +62,14 @@ public class Spawner : MonoBehaviour
     {
         float xPos = Random.Range(transform.position.x - spawnOffset, transform.position.x + spawnOffset);
         float zPos = Random.Range(transform.position.z - spawnOffset, transform.position.z + spawnOffset);
-        Instantiate(coin, new Vector3(xPos, transform.position.y, zPos), transform.rotation);
+        Ray ray = new Ray(new Vector3(xPos, 0, zPos), transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            Vector3 pos = hit.point;
+            pos.y += 2.0f;
+            GameObject c = Instantiate(coin, pos, transform.rotation);
+            c.transform.SetParent(hit.collider.transform);
+        }
     }
 }
